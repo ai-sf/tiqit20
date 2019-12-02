6d6fdf00a2d4e67677a7a28bf98f37ad0e7554f2
CiccaGuru <robyciccarelli.rc@gmail.com>
2019-12-02 21:31:03 +0100

    Better oraganise code

--- js/init.js
+++ js/init.js
@@ -1,8 +1,29 @@
 (function($){
-  $(function(){
+    $(function(){
+        M.AutoInit()
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+        $('.slider').slider({full_width: false});
+        // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+        $('.collapsible').collapsible({
+            onOpenStart: function(el){
+                $(".collapsible-header .material-icons", el).text("keyboard_arrow_up");
+            },
+            onCloseStart: function(el){
+                $(".collapsible-header .material-icons", el).text("keyboard_arrow_down");
+            }
+        });
+        $('.modal').modal();
+        $('.carousel').carousel();
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+        $(".row").each(function(){
+            maxHeight = 0;
+            $(".card .card-content", $(this)).each(function(){
+                maxHeight = Math.max($(".card-title", this).height(), maxHeight);
+            });
+            $(".card .card-content", $(this)).each(function(){
+                $(this).height(maxHeight);
+            });
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
